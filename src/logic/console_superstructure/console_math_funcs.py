87bc54b3d682e5f09b066bd075cdd1e10ad48867
LeeryLis <64995672+LeeryLis@users.noreply.github.com>
2025-05-20 19:19:38 +0300

    Update console_math_funcs.py

--- src/logic/console_superstructure/console_math_funcs.py
+++ src/logic/console_superstructure/console_math_funcs.py
@@ -10,10 +10,18 @@ class ConsoleMathFuncs(BasicConsole):
         self.console = Console()
 
     def _register_commands(self, console_manager: ConsoleManager) -> None:
+        param_add_text = Param(
+            description="Добавить текст в вывод",
+            action=lambda title: self.console.print(Text(title.replace("\\n", "\n")), end=""),
+            usage="-text <text>",
+            param_type=ParamType.NO_MODIFY,
+            arg_number=1
+        )
+
         console_manager.register_command(
-            action=self.original.sum_func,
             aliases=["sum"],
             description="Суммировать произвольное количество целых чисел",
+            action=self.original.sum_func,
             usage="sum [param_1] ... [param_N] <int_1> ... <int_N>",
             params={
                 "-s": Param(
@@ -52,29 +60,67 @@ class ConsoleMathFuncs(BasicConsole):
                     param_type=ParamType.ARG_MODIFY,
                     arg_number=2
                 ),
-                "-text": Param(
-                    description="Добавить текст в вывод",
-                    action=lambda title: Text(title.replace("\\n", "\n")),
-                    usage="-text <text>",
-                    param_type=ParamType.NO_MODIFY,
-                    arg_number=1
-                ),
+                "-text": param_add_text,
                 "-sort": Param(
                     description="Отсортировать числа по возрастанию",
                     action=lambda *numbers: sorted(numbers),
                     param_type=ParamType.ARG_MODIFY
+                )
+            }
+        )
+        console_manager.register_command(
+            aliases=["rand"],
+            description="Сгенерировать список случайных чисел",
+            action=lambda: Text(f"Необходимо ввести параметры", style="red"),
+            usage="rand <param_1> [param_2] ... [param_N]",
+            params={
+                "-sort": Param(
+                    description="Отсортировать числа по возрастанию",
+                    action=lambda numbers: sorted(numbers),
+                    param_type=ParamType.RESULT_MODIFY
                 ),
-                "-sub": Param(
-                    description="Вычесть число из результата",
-                    action=self.sub_number,
-                    param_type=ParamType.RESULT_MODIFY,
-                    arg_number=1
+                "-text": param_add_text,
+                "-uniform": Param(
+                    description="Использовать равномерное распределение",
+                    action=self.generate_random_numbers_uniform,
+                    usage="-uniform <count> <min value> <max value>",
+                    param_type=ParamType.LOGIC,
+                    arg_number=3
+                ),
+                "-normal": Param(
+                    description="Использовать нормальное распределение",
+                    action=self.generate_random_numbers_normal,
+                    usage="-normal <count> <mean> <std_dev>",
+                    param_type=ParamType.LOGIC,
+                    arg_number=3
+                ),
+                "-exp": Param(
+                    description="Использовать экспоненциальное распределение",
+                    action=self.generate_random_numbers_exponential,
+                    usage="-exp <count> <scale>",
+                    param_type=ParamType.LOGIC,
+                    arg_number=2
                 )
             }
         )
 
-    def sub_number(self, number: int, result: int) -> int:
-        return result - number
+    def generate_random_numbers_uniform(self, count: int, min_value: int, max_value: int) -> list[int] | Text:
+        try:
+            return self.original.generate_random_numbers_uniform(count, min_value, max_value)
+        except ValueError as e:
+            return Text(f"ValueError: {e}")
+
+    def generate_random_numbers_normal(self, count: int, mean: int, std_dev: int) -> list[int] | Text:
+        try:
+            return self.original.generate_random_numbers_normal(count, mean, std_dev)
+        except ValueError as e:
+            return Text(f"ValueError: {e}")
+
+    def generate_random_numbers_exponential(self, count: int, scale: float) -> list[int] | Text:
+        try:
+            return self.original.generate_random_numbers_exponential(count, scale)
+        except ValueError as e:
+            return Text(f"ValueError: {e}")
 
 
 if __name__ == "__main__":
