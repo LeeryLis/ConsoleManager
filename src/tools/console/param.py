74c4854ace05aa218b260209d30d62928dcf79c2
LeeryLis <64995672+LeeryLis@users.noreply.github.com>
2025-05-20 04:03:18 +0300

    Update param.py

--- src/tools/console/param.py
+++ src/tools/console/param.py
@@ -1,54 +1,120 @@
-from typing import Callable,Optional, Any
+import inspect
+from typing import Callable, Any, Sequence
+from src.tools.console.param_type import ParamType
+from rich.text import Text
 
 
 class Param:
     def __init__(
             self,
             *,
-            description: str,
             action: Callable[..., Any],
-            modify: bool = True,
-            execute_command_action: bool = True,
-            return_result: bool = False,
-            use_command_args: bool = True,
-            arg_types: Optional[list[type]] = None,
-            usage: str = ''
-    ) -> None:
+            description: str,
+            param_type: ParamType,
+            usage: str = '',
+            arg_number: int = 0
+    ):
         """
         Параметр команды. Нужен для организации вариаций одной и той же команды или модификации переданных
         в команду аргументов.
         Имя параметра не представлено, так как это подчёркивает, что любой параметр
         неразрывно связан со своей командой и должен храниться в собственном словаре команды.
 
-        :param description: Описание параметра для пользователя
         :param action: Вызываемая параметром функция
-        :param modify: Параметр, показывающий, будут ли модифицированы введённые в команду аргументы
-        (передаваемые в функцию команды)
-        Если параметр modify=False, то он не изменяет передаваемые в основную функцию в логике команды-родителя
-        аргументы, а, например, производит вывод в консоль или модифицированный вывод в консоль.
-        Например: sum - суммирует числа и выводит результат, -s - выводит слагаемые
-        sum -s 1 2 3 >>> [1, 2, 3] 6
-        Или: -p - фильтрует числа, оставляя только положительные (не выводит в консоль, работает только с логикой)
-        1. sum -p -s 1 -2 3 -4 -5 >>> [1, 3] 4 - если для -p modify=True
-        2. sum -p -s 1 -2 3 -4 -5 >>> [1, -2, 3, -4, -5] -7 - если для -p modify=False
-        Во втором случае работа параметра не повлияла на реальные аргументы для основной функции
-        :param execute_command_action: если хотя бы у одного из параметров
-        execute_command_action был равен False, то в самом конце, после выполнения функций всех параметров,
-        функция команды не будет выполнена
-        :param return_result: если True, то результат выполнения функции параметра будет добавлен в общий result
-        :param use_command_args: будет ли функция параметра принимать на вход аргументы, введённые
-        для функции команды (например, для их модификации или вывода в консоль)
-        :param arg_types: Типы аргументов, используемых параметром
-        Например: -lb <int> - фильтрует числа по установленной нижней границе
-        sum -lb 5 -s 1 3 5 7 9 >>> [5, 7, 9] 21
+        :param description: Описание параметра для пользователя
+        :param param_type: Тип параметра. В файле param_type.py можно узнать подробнее о типах. Типы используются
+        для правильной организации порядка выполнения параметров
         :param usage: Описание использования для пользователя
         Например: "-lb <lower bound>", "-b <lower bound> <upper bound>"
+        :param arg_number: Используется для правильного получения требуемых аргументов
         """
-        self.description = description
         self.action = action
-        self.modify = modify
-        self.execute_command_action = execute_command_action
-        self.return_result = return_result
-        self.use_command_args = use_command_args
-        self.arg_types = arg_types
+        self.description = description
         self.usage = usage
+        self.param_type = param_type
+        self.arg_number = arg_number
+
+    def _get_argspec(self) -> tuple[inspect.FullArgSpec, list[str]]:
+        argspec = inspect.getfullargspec(self.action)
+        argspec_args = argspec.args[1:] if argspec.args[0] == 'self' else argspec.args
+        return argspec, argspec_args
+
+    @staticmethod
+    def _convert_positional_args(
+            args: Sequence[str], argspec: inspect.FullArgSpec, argspec_args: list[str]
+    ) -> tuple[list[Any], int]:
+        converted_args = []
+        args_index = 0
+        for arg_name in argspec_args:
+            if args_index < len(args):
+                arg = args[args_index]
+                annotation = argspec.annotations.get(arg_name, None)
+                if annotation:
+                    converted_arg = annotation(arg)
+                else:
+                    converted_arg = arg
+                converted_args.append(converted_arg)
+                args_index += 1
+            else:
+                if argspec.defaults and arg_name in argspec.args[-len(argspec.defaults):]:
+                    default_index = argspec.args.index(arg_name) - len(argspec.args) + len(argspec.defaults)
+                    converted_args.append(argspec.defaults[default_index])
+                else:
+                    raise TypeError(f"Не передан обязательный аргумент {arg_name}")
+        return converted_args, args_index
+
+    @staticmethod
+    def _convert_varargs(args: Sequence[str], argspec: inspect.FullArgSpec, args_index: int) -> list[Any]:
+        if argspec.varargs:
+            varargs_name = argspec.varargs
+            varargs_annotation = argspec.annotations.get(varargs_name, None)
+            if varargs_annotation and varargs_annotation != Any:
+                converted_varargs = [varargs_annotation(arg) for arg in args[args_index:]]
+            else:
+                converted_varargs = args[args_index:]
+            return converted_varargs
+        else:
+            return []
+
+    @staticmethod
+    def _convert_kwonlyargs(args: Sequence[str], argspec: inspect.FullArgSpec, args_index: int) -> list[Any]:
+        if argspec.kwonlyargs:
+            converted_args = []
+            for arg_name in argspec.kwonlyargs:
+                if args_index < len(args):
+                    arg = args[args_index]
+                    annotation = argspec.annotations.get(arg_name, None)
+                    if annotation:
+                        converted_arg = annotation(arg)
+                    else:
+                        converted_arg = arg
+                    converted_args.append(converted_arg)
+                    args_index += 1
+                else:
+                    if arg_name in argspec.kwonlydefaults:
+                        converted_args.append(argspec.kwonlydefaults[arg_name])
+                    else:
+                        raise TypeError(f"Не передан обязательный ключевой аргумент {arg_name}")
+            return converted_args
+        else:
+            return []
+
+    def convert_args(self, args: Sequence[str]) -> list[Any]:
+        argspec, argspec_args = self._get_argspec()
+        converted_args, args_index = self._convert_positional_args(args, argspec, argspec_args)
+        converted_varargs = self._convert_varargs(args, argspec, args_index)
+        converted_kwonlyargs = self._convert_kwonlyargs(args, argspec, args_index)
+        converted_args.extend(converted_varargs)
+        converted_args.extend(converted_kwonlyargs)
+        return converted_args
+
+    def execute(self, *args) -> Any | Text:
+        try:
+            converted_args = self.convert_args(args) if self.arg_number else args
+            return self.action(*converted_args)
+        except (ValueError, TypeError) as ex:
+            result = Text(f"{ex}\n", style="red")
+            if self.usage:
+                result.append("Usage: ", style="green")
+                result.append(f"{self.usage}", style="white")
+            return result
