40e9ccf0313757a9f648142b9cf315cc121c4d68
LeeryLis <64995672+LeeryLis@users.noreply.github.com>
2025-05-20 19:16:17 +0300

    Update math_funcs.py

--- src/logic/math_funcs.py
+++ src/logic/math_funcs.py
@@ -1,4 +1,25 @@
+import random
+import numpy as np
+
+
 class MathFuncs:
     def sum_func(self, *numbers: int) -> int:
         return sum(numbers)
-    
+
+    def generate_random_numbers_uniform(self, count: int, min_value: int, max_value: int) -> list[int] | ValueError:
+        if count <= 0:
+            raise ValueError("Количество чисел должно быть положительным")
+
+        return [int(round(random.uniform(min_value, max_value))) for _ in range(count)]
+
+    def generate_random_numbers_normal(self, count: int, mean: int, std_dev: int) -> list[int] | ValueError:
+        if count <= 0:
+            raise ValueError("Количество чисел должно быть положительным")
+
+        return [int(value) for value in np.random.normal(mean, std_dev, count)]
+
+    def generate_random_numbers_exponential(self, count: int, scale: float) -> list[int] | ValueError:
+        if count <= 0:
+            raise ValueError("Количество чисел должно быть положительным")
+
+        return [int(value) for value in np.random.exponential(scale, count)]
