4f9270baaa8f99a23785df0492db4451e0a5fdba
LeeryLis <64995672+LeeryLis@users.noreply.github.com>
2025-05-20 03:53:15 +0300

    Update console_math_funcs.py

--- src/logic/console_superstructure/console_math_funcs.py
+++ src/logic/console_superstructure/console_math_funcs.py
@@ -1,70 +1,76 @@
-from src.tools.console import BasicConsole
-from src.tools.console import ConsoleManager, Command, Param
+from rich.text import Text
+from rich.console import Console
 
 from src.logic import MathFuncs
-
-from rich.text import Text
+from src.tools.console import BasicConsole, ConsoleManager, Param, ParamType
 
 class ConsoleMathFuncs(BasicConsole):
     def __init__(self, original: MathFuncs):
         self.original = original
+        self.console = Console()
 
     def _register_commands(self, console_manager: ConsoleManager) -> None:
-        console_manager.register_command(Command(
+        console_manager.register_command(
+            action=self.original.sum_func,
             aliases=["sum"],
             description="Суммировать произвольное количество целых чисел",
-            action=self.original.sum_func,
             usage="sum [param_1] ... [param_N] <int_1> ... <int_N>",
-            arg_types=[int],
-            accepts_varargs=True,
             params={
                 "-s": Param(
                     description="Вывести числа, участвующие в суммировании",
-                    action=lambda *numbers: numbers,
-                    modify=False,
-                    return_result=True
+                    action=lambda *numbers: self.console.print(numbers),
+                    param_type=ParamType.NO_MODIFY
                 ),
                 "-n": Param(
                     description="Отфильтровать: только отрицательные числа",
-                    action=lambda *numbers: list(filter(lambda n: n < 0, numbers))
+                    action=lambda *numbers: list(filter(lambda n: n < 0, numbers)),
+                    param_type=ParamType.ARG_MODIFY
                 ),
                 "-p": Param(
                     description="Отфильтровать: только положительные числа",
-                    action=lambda *numbers: list(filter(lambda n: n > 0, numbers))
+                    action=lambda *numbers: list(filter(lambda n: n > 0, numbers)),
+                    param_type=ParamType.ARG_MODIFY
                 ),
                 "-lb": Param(
                     description="Отфильтровать: по нижней границе",
                     action=lambda lb, *numbers: list(filter(lambda n: n >= lb, numbers)),
-                    arg_types=[int],
-                    usage="-lb <lower bound>"
+                    usage="-lb <lower bound>",
+                    param_type=ParamType.ARG_MODIFY
                 ),
                 "-ub": Param(
                     description="Отфильтровать: по верхней границе",
                     action=lambda ub, *numbers: list(filter(lambda n: n <= ub, numbers)),
-                    arg_types=[int],
-                    usage="-ub <upper bound>"
+                    usage="-ub <upper bound>",
+                    param_type=ParamType.ARG_MODIFY
                 ),
                 "-b": Param(
                     description="Отфильтровать: по нижней и верхней границе",
                     action=lambda lb, ub, *numbers: list(filter(lambda n: lb <= n <= ub, numbers)),
-                    arg_types=[int, int],
-                    usage="-b <lower bound> <upper bound>"
+                    usage="-b <lower bound> <upper bound>",
+                    param_type=ParamType.ARG_MODIFY
                 ),
                 "-text": Param(
                     description="Добавить текст в вывод",
                     action=lambda title: Text(title.replace("\\n", "\n")),
                     usage="-text <text>",
-                    arg_types=[str],
-                    modify=False,
-                    return_result=True,
-                    use_command_args=False
+                    param_type=ParamType.NO_MODIFY
                 ),
                 "-sort": Param(
                     description="Отсортировать числа по возрастанию",
-                    action=lambda *numbers: sorted(numbers)
+                    action=lambda *numbers: sorted(numbers),
+                    param_type=ParamType.ARG_MODIFY
+                ),
+                "-sub": Param(
+                    description="Вычесть число из результата",
+                    action=self.sub_number,
+                    param_type=ParamType.RESULT_MODIFY,
+                    arg_number=1
                 )
             }
-        ))
+        )
+
+    def sub_number(self, number: int, result: int) -> int:
+        return result - number
 
 
 if __name__ == "__main__":
